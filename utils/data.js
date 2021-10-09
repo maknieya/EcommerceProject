a4ffcbfe9336de1b6253af720ad9af882a3f5ad5
maknieya <67949549+maknieya@users.noreply.github.com>
2021-10-09 10:40:42 +0100

    Add products

---
+++ utils/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Free Shirt',
+      category: 'Shirts',
+      image:
+        'https://www.peaksports.tn/15870-home_default/t-shirts-running-plus-orange.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 10,
+      description: 'A popular shirt',
+    },
+    {
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image:
+        'https://www.peaksports.tn/15870-home_default/t-shirts-running-plus-orange.jpg',
+      price: 80,
+      brand: 'Nike',
+      rating: 6,
+      numReviews: 10,
+      countInStock: 10,
+      description: 'A popular shirt',
+    },
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image:
+        'https://www.peaksports.tn/15870-home_default/t-shirts-running-plus-orange.jpg',
+      price: 100,
+      brand: 'Nike',
+      rating: 4.6,
+      numReviews: 15,
+      countInStock: 17,
+      description: 'A popular shirt',
+    },
+    {
+      name: 'Golf Pants',
+      category: 'Pants',
+      image:
+        'https://www.peaksports.tn/15870-home_default/t-shirts-running-plus-orange.jpg',
+      price: 78,
+      brand: 'Aara',
+      rating: 6.9,
+      numReviews: 8,
+      countInStock: 10,
+      description: 'A popular shirt',
+    },
+  ],
+};
+export default data;
